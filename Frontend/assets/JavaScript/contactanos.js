56f1c94a6d904c99c7c808bb688da11ecc6335ce
Axel Sara <axelsaragar@gmail.com>
2024-04-08 10:55:40 -0600

    actualización de repo

--- Frontend/assets/JavaScript/contactanos.js
+++ Frontend/assets/JavaScript/contactanos.js
@@ -1,21 +1,5 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
-<<<<<<< HEAD
-    const form = document.querySelector('form');
-
-    // const sgMail = require('@sendgrid/mail');
-    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-
-        // validar los inputs del formulario con expresiones regulares
-        const validar = validarInputs(...form.elements);
-
-=======
     const form = document.querySelector('.formContactanos');
     const contenedorErrores = document.querySelector('.contenedor_errores_contactanos');
 
@@ -26,48 +10,32 @@ document.addEventListener('DOMContentLoaded', () => {
         // Obtener los valores de los campos de entrada
         const nombre = formData.get('nombreContactanos');
         const email = formData.get('emailContactanos');
+        const telefono = formData.get('telefonoContactanos');
         const comentario = formData.get('comentarioContactanos');
 
         // validar los inputs del formulario con expresiones regulares
-        const validar = validarInputs(nombre, email, comentario);
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
+        const validar = validarInputs(nombre, email,telefono, comentario);
 
         if (!validar) {
             return;
         }
         else {
-<<<<<<< HEAD
-            // Obtener los datos del formulario
-            const formData = new FormData(form);
-            formData.forEach((value, key) => {
-                data[key] = value;
-            });
-=======
             const requestData = {
                 nombre: nombre,
                 email: email,
                 comentario: comentario
             };
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
 
             const options = {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-<<<<<<< HEAD
-                body: JSON.stringify(formData)
-              };
-
-            try {
-                const response = fetch('https://formsubmit.co/alanbc60@gmail.com', options)     
-=======
                 body: JSON.stringify(requestData)
               };
 
             try {
                 const response = await fetch('https://formsubmit.co/submit', options);
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
                 const data = response.json();
     
                 if(response.ok) {
@@ -75,12 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     // crear un modal de exito
                 }
                 else {
-<<<<<<< HEAD
-                    console.log("Error al enviar el email");
-                }                
-            } catch (error) {
-                console.log(error);
-=======
                     contenedorErrores.innerHTML = `
                     <div class="alert alert-danger" role="alert">
                         ${data.error}
@@ -93,7 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     Error al enviar la solicitud. Por favor, inténtelo de nuevo más tarde.
                 </div>
                 `;
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
             }
         }
 
@@ -101,28 +62,14 @@ document.addEventListener('DOMContentLoaded', () => {
 }); 
 
 
-async function validarInputs(nombre, email, comentario) {
+async function validarInputs(nombre, email,telefono, comentario) {
 
-<<<<<<< HEAD
-    const contenedorformulario = document.querySelector('.contenedor_formulario_contactanos');
-
-    let validacion = true;
-
-    // validar que el nombre solo contenga letras y espacios
-    let regex = /^[a-zA-Z]+$/;
-
-    // validar que el correo solo contenga letras, numeros, puntos, guiones y guion bajo
-    let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    
-    if (nombre.value === "" || email.value === "" || comentario.value === "") {
-        validacion = false;
-        alert("Todos los campos son obligatorios.");
-    }
-=======
 
     console.log("Nombre: " + nombre);
     console.log("Email: " + email);
+    console.log("telefono: "+telefono)
     console.log("Comentario: " + comentario);
+    
     const contenedorErrores = document.querySelector('.contenedor_errores_contactanos');
 
     let validacion = true;
@@ -132,20 +79,19 @@ async function validarInputs(nombre, email, comentario) {
 
     // validar que el correo solo contenga letras, numeros, puntos, guiones, guion bajo y arroba
     let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    // validar el numero de telefono
+
+    let regexTel = /^[0-9]+$/;
+
     
-
     // limpiar el contenedor de errores
-
     contenedorErrores.innerHTML = '';
-
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
 
     if (!regex.test(nombre)) {
         validacion = false;
         // crear un contenedor de error
-<<<<<<< HEAD
-        alert("El nombre contiene caracteres no válidos.");  
-=======
         contenedorErrores.innerHTML += `
         <div class="alert alert-danger" role="alert">
             El nombre contiene caracteres no válidos.
@@ -158,16 +104,10 @@ async function validarInputs(nombre, email, comentario) {
             contenedorErrores.innerHTML = '';
         }, 4000)
 
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
     } 
 
     if(!regexEmail.test(email)) {
         validacion = false;
-<<<<<<< HEAD
-        alert("El correo contiene caracteres no válidos, solo acepta letras, números, puntos, guiones y guion bajo.");
-    }
-
-=======
         
         // crear un contenedor de error
         contenedorErrores.innerHTML += `
@@ -183,42 +123,54 @@ async function validarInputs(nombre, email, comentario) {
         }, 4000)
     }
 
+    // validar que sean solo numeros 
+    if (telefono.length != 10 ) {
+        validacion = false;
+        contenedorErrores.innerHTML += `
+        <div class="alert alert-danger" role="alert">
+            El telefono debe contener 10 numeros.
+        </div>
+        `;
+
+        // Limpiar el contenedor de errores
+        setTimeout(() => {
+            contenedorErrores.innerHTML = '';
+        }, 4000)
+    }
+
+
 
     // validacion del telefono
-    if (comentario.value.length < 10) {
+    if (comentario.length < 10) {
         validacion = false;
         contenedorErrores.innerHTML += `
         <div class="alert alert-danger" role="alert">
             El telefono debe tener al menos 10 números.
         </div>
         `;
+
+        // Limpiar el contenedor de errores
+        setTimeout(() => {
+            contenedorErrores.innerHTML = '';
+        }, 4000)
     }
 
-
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
     // validar que el comentario no exceda los 255 caracteres
-    if (comentario.value.length > 255) {
+    if (comentario.length > 255) {
         validacion = false;
         alert("El comentario no puede tener más de 255 caracteres.");
     }
 
-<<<<<<< HEAD
-=======
 
 
 
 
 
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
     return validacion;
 
 }
 
 
-<<<<<<< HEAD
-
-
-=======
 function mostrarMensajeDeError(mensaje, tiempoVisible = 3000) {
     console.log("Entro a mostrar el error");
     const toast = Swal.mixin({
@@ -242,7 +194,6 @@ function mostrarMensajeDeError(mensaje, tiempoVisible = 3000) {
     });
   }
   
->>>>>>> bbc3f5d97e80a0cb017653b6bcd5e76108be9c8b
 
 
 
--- Frontend/assets/JavaScript/slider.js
+++ Frontend/assets/JavaScript/slider.js
@@ -1,9 +1,7 @@
 const banner = () => {
-    
+
 }
-
 banner();
-
 (function () {
     "use strict";
 
@@ -26,7 +24,6 @@ banner();
                         behavior: "smooth"
                     });
 
-
                     // Set active context item
                     const active_context_item = context_item.closest(".slide_navigation").querySelector(".active");
                     if (active_context_item) {
@@ -47,8 +44,8 @@ banner();
                 Object.keys(navigation_items).forEach(function (key) {
                     navigation_items[key].onclick = function (e) {
                         e.preventDefault();
-
                         vertical_slider.show_slide(key, navigation_items[key]);
+                        indexBanner();
                     };
                 });
             }
@@ -67,3 +64,111 @@ banner();
     // Initialize sliders
     vertical_slider.init();
 }());
+
+const dataProductos = async (url) => {
+    const response = await fetch("./Frontend/assets/json/productos.json");
+    const data = await response.json();
+    findData(data, url)
+}
+
+const findData = (data, url) => {
+    return data.map(dat => {
+        if (url === dat.imgMuestra) {
+            document.getElementById("Modelo").innerHTML = dat.modelo;
+            document.getElementById("Color").innerHTML = dat.color;
+            document.getElementById("price").innerHTML = `$${dat.precio}.00`;
+            document.getElementById("marca").innerHTML = dat.marca;
+        }
+    });
+};
+
+// Initialize a img for each slide
+
+const imgInit = async () => {
+    const response = await fetch("./Frontend/assets/json/productos.json");
+    const data = await response.json();
+    showImg(data, 0);
+    const imgElements = document.querySelectorAll(".img-pdt img");
+    findData(data, imgElements[0].src);
+}
+
+const showImg = (data, id) => {
+    let banner = "";
+    return data.map(dat => {
+        if (0 === dat.id) {
+            banner += `
+            <div class="slide">
+                <div class="img-pdt inner_content">
+                    <img class="img-banner" src="${data[0].imgMuestra}" alt="" id="img_1">
+                </div>
+            </div>
+            `;
+        } else if (6 === dat.id) {
+            banner += `
+            <div class="slide">
+                <div class="img-pdt inner_content">
+                    <img class="img-banner" src="${data[6].imgMuestra}" alt="" id="img_1">
+                </div>
+            </div>
+            `;
+        } else if (10 === dat.id) {
+            banner += `
+            <div class="slide">
+                <div class="img-pdt inner_content">
+                    <img class="img-banner" src="${data[10].imgMuestra}" alt="" id="img_1">
+                </div>
+            </div>
+            `;
+        }
+        document.getElementById("img__banner").innerHTML = banner;
+    });
+}
+
+imgInit();
+
+const indexBanner = () => {
+
+    const imgElements = document.querySelectorAll(".img-pdt img");
+
+    const enlaces = document.querySelectorAll('li');
+    enlaces.forEach(function (enlace) {
+        enlace.addEventListener('click', function (event) {
+            event.preventDefault();
+            let id = this.id;
+            if(id == "li1"){
+                dataProductos(imgElements[0].src);
+            } else if(id == "li2"){
+                dataProductos(imgElements[1].src);
+            } else if(id == "li3"){
+                dataProductos(imgElements[2].src);
+            } 
+        });
+    });
+}
+
+let cart = [];
+const AddCarritoBanner = async () => {
+    const modelo = document.getElementById("Modelo").innerHTML;
+    const color = document.getElementById("Color").innerHTML;
+    const response = await fetch("./Frontend/assets/json/productos.json");
+    const data = await response.json();
+    for (const product of data) {
+        if(modelo === product.modelo && color === product.color){
+            cart.push( {
+                "id": product.id,
+                "modelo": product.modelo,
+                "color": product.color,
+                "talla": 7,
+                "precio": product.precio,
+                "marca": product.marca,
+                "imgMuestra": product.imgMuestra,
+                "imgLateral": product.imgLateral,
+                "imgFrontal": product.imgFrontal,
+                "imgSuperior": product.imgSuperior
+            });
+        }
+    }
+    localStorage.setItem("carrito", JSON.stringify(cart));
+    const carritoLocalStorage = localStorage.getItem("carrito");
+    console.log(carritoLocalStorage);
+}
